2684753afda497bd1dcff7a180dee17739e93d99
BrianCobianS <98795587+BrianCobianS@users.noreply.github.com>
2022-07-28 10:33:08 -0500

    Update ola.groovy

--- ola.groovy
+++ ola.groovy
@@ -1,20 +1,44 @@
-job('ejemplo2-job-DSL') {
-	description('Job DSL de ejemplo para el curso de Jenkins')
-  	scm {
-      		git('https://github.com/macloujulian/jenkins.job.parametrizado.git', 'main') { node ->
-        		node / gitConfigName('macloujulian')
-        		node / gitConfigEmail('macloujulian@gmail.com')
-      		}
-    	} 
-  	parameters {
-   		stringParam('nombre', defaultValue = 'Julian', description = 'Parametro de cadena para el Job Booleano')
-      		choiceParam('planeta', ['Mercurio', 'Venus', 'Tierrra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno'])
-      		booleanParam('agente', false)
-    	}
-  	triggers {
-    		cron('H/7 * * * *')
-    	}
-  	steps {
-    		shell("bash jobscript.sh")
-    	}
+pipeline {
+    agent any
+    stages {
+        stage ('Checkout Project') {
+            steps {
+                checkout scm
+                sh "echo ' ' > /root/.ssh/known_hosts"
+            }
+        }
+  
+        stage ('Show Values') {
+            steps {
+                sh "echo Test case to run: "
+                sh "echo Inventory: $inventory"
+                sh "echo level: $level_name"
+                sh "echo level Nexus Name: $level_complement"
+                sh "echo opcion: $opc"
+            }
+        }
+
+        stage ('Run_Playbook Import_Inventory') {
+            when {
+                beforeAgent true
+                expression { inventory == 'Upload_inventory' }
+            }
+            steps {
+                sh "echo '$import_inventory' > Inventories/import_inventory.yml"
+                sh "ansible-playbook playbooks/os4690/Install_Controller.yml -vv  -i Inventories/import_inventory.yml -e 'level_complement=$level_complement opc=$opc level_name=$level_name'"
+            }
+        }
+        
+        stage ('Post playbook') {
+            steps {
+                sh "echo Test case to run: "
+                sh "echo Done!"
+            }
+        }
+    }
+    post { 
+        always { 
+            cleanWs()
+        }
+    }
 }
